661714dccaac58e14f14d72cae34b3a816dcf916
DiiD1999 <DiiD1999@outlook.com>
2023-03-06 10:51:27 +0800

    feat(Example-Quartrz): 添加Quatrz 简单定时任务样例

--- Example-Quartrz/src/test/java/com/example/quartz/QuartzApplicationTests.java
+++ Example-Quartrz/src/test/java/com/example/quartz/QuartzApplicationTests.java
@@ -1,13 +1,39 @@
 package com.example.quartz;
 
+import com.example.quartz.utils.HelloJob;
 import org.junit.jupiter.api.Test;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class QuartzApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws SchedulerException {
+
+        //创建一个scheduler
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.getContext().put("skey", "svalue");
+
+        //创建一个Trigger
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("trigger1", "group1")
+                .usingJobData("t1", "tv1")
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(3)
+                        .repeatForever()).build();
+        trigger.getJobDataMap().put("t2", "tv2");
+
+        //创建一个job
+        JobDetail job = JobBuilder.newJob(HelloJob.class)
+                .usingJobData("j1", "jv1")
+                .withIdentity("myjob", "mygroup").build();
+        job.getJobDataMap().put("j2", "jv2");
+
+        //注册trigger并启动scheduler
+        scheduler.scheduleJob(job,trigger);
+        scheduler.start();
     }
+
 
 }
