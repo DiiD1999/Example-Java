ab93204aacd472ab6f6270448762b2234eff5893
DiiD1999 <DiiD1999@outlook.com>
2023-03-10 10:38:13 +0800

    feat(Example-Thread): 将线程设置为守护线程与其显示结果

--- Example-Thread/src/main/java/Main.java
+++ Example-Thread/src/main/java/Main.java
@@ -4,9 +4,14 @@ import java.util.concurrent.FutureTask;
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Thread thread1 = new ThreadOne();
+        // 将子线程设置为守护线程, 使得主线程在退出的时候，JVM会关闭所有守护线程
+        // 如果setDaemon在start后，会出现IllegalThreadStateException错误
+        thread1.setDaemon(true);
         thread1.start();
 
         Thread thread2 = new Thread(new ThreadTwo());
+        // 设置为守护线程
+        thread2.setDaemon(true);
         thread2.start();
 
         ThreadThree thread3 = new ThreadThree();
@@ -17,6 +22,6 @@ public class Main {
         System.out.println(sum);
 
 
-//        Thread.sleep(500);
+        Thread.sleep(500);
     }
 }
--- Example-Thread/src/main/java/ThreadOne.java
+++ Example-Thread/src/main/java/ThreadOne.java
@@ -7,6 +7,12 @@ public class ThreadOne extends  Thread {
 
     @Override
     public void run() {
+        try{
+            Thread.sleep(500);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
             if (i == Integer.MAX_VALUE >> 4) {
                 // 测试主线程存在，子线程是否还存在。事实证明还存在
